48ef84bee3c4063f9af94dcb48cea45aae640677
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-28 21:31:06 +0300

    ex

---
+++ 01.Inheritance/Lab/04.RandomList/04.RandomList/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CustomRandomList
+{
+	public class RandomList : List<string>
+	{
+		public Random R { get; set; }
+
+		public RandomList()
+		{
+			R = new Random();
+		}
+		public string RandomString()
+		{
+			string removedElement = this[R.Next(0, Count)];
+
+			this.Remove(removedElement);
+
+			return removedElement;
+		}
+	}
+}
+
