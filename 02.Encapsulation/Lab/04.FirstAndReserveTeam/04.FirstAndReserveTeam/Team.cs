b772bb6710b72e8ee70fe92ee7b56252e071b2f2
Hristo Papanov <hristopapanov13@gmail.com>
2022-10-31 20:56:20 +0200

    ex

---
+++ 02.Encapsulation/Lab/04.FirstAndReserveTeam/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PersonsInfo
+{
+	public class Team
+	{
+		private string name;
+		private List<Person> firstTeam;
+		private List<Person> reserveTeam;
+
+		public string Name { get; set;  }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        public Team(string name)
+		{
+			Name = name;
+			firstTeam = new List<Person>();
+			reserveTeam = new List<Person>();
+		}
+
+		public void AddPerson(Person person)
+		{
+			if(person.Age<40)
+			{
+				firstTeam.Add(person);
+			}
+			else
+			{
+				reserveTeam.Add(person);
+			}
+		}
+	}
+}
+
