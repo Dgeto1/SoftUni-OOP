968f5e126789192ff601930fd7fdc0c40fe6e966
Hristo Papanov <hristopapanov13@gmail.com>
2022-11-01 17:23:05 +0200

    ex

---
+++ 02.Encapsulation/Exercise/01.ClassBoxData/01.ClassBoxData/Box.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace _01.ClassBoxData
+{
+	public class Box
+	{
+        private double length, width, height;
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+		{
+			get { return this.length; }
+			private set
+			{
+                if (value <= 0) throw new ArgumentException($"Lenght cannot be zero or negative.");
+                this.length = value;
+			}
+		}
+
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                if (value <= 0) throw new ArgumentException($"Width cannot be zero or negative.");
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            private set
+            {
+                if (value <= 0) throw new ArgumentException($"Height cannot be zero or negative.");
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * (Length * Height + Length * Width + Height * Width);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * (Length * Height + Width * Height);
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        public override string ToString()
+        {
+            return $"Surface Area - {this.SurfaceArea():f2}" + Environment.NewLine+
+                $"Lateral Surface Area - {this.LateralSurfaceArea():f2}" + Environment.NewLine +
+                $"Volume - {this.Volume():f2}";
+        }
+    }
+}
+
---
+++ 02.Encapsulation/Exercise/01.ClassBoxData/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine(box.ToString());
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            
+        }
+    }
+}
+
